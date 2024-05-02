97643a3b1b9a2233ab2f75927b964b3ca1eea429
lauLanez <laulanyumbayire@gmail.com>
2024-05-02 14:17:37 -0500

    search bar added

--- client/src/Components/Dashboard/Components/Body Section/Top Section/Top.jsx
+++ client/src/Components/Dashboard/Components/Body Section/Top Section/Top.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './top.css'
+import { BiSearchAlt } from "react-icons/bi";
 
 const Top = () => {
   return (
-    <div>Top</div>
+    <div className='topSection'>
+      <div className="headerSection flex">
+        <div className="title">
+          <h1>Welcome to Hinga .</h1>
+          <p>Hello Farmer, Welcome back !</p>
+        </div>
+
+        <div className="searchBar flex">
+          <input type="text"  placeholder='Search Dashboard'/>
+          <BiSearchAlt className='icon'/>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
